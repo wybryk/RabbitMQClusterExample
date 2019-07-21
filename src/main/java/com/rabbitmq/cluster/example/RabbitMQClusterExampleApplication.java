142b0d3b4a418c4e0d9f752041615dad36cd70a1
Mateusz Maciąg <mateuszmaciag10@gmail.com>
2019-07-21 20:36:33 +0200

    Project init

---
+++ src/main/java/com/rabbitmq/cluster/example/RabbitMQClusterExampleApplication.java
@@ -0,0 +1,11 @@
+package com.rabbitmq.cluster.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMQClusterExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQClusterExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rabbitmq/cluster/example/RabbitMQClusterExampleApplicationTest.groovy
@@ -0,0 +1,11 @@
+package com.rabbitmq.cluster.example
+
+import org.junit.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RabbitMQClusterExampleApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
